7ee4a392773266711047207668435402780f7c0f
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-13 04:14:57 +0200

    Contexto

---
+++ RegistroDeBiblias/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using RegistroDeBiblias.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDeBiblias.DAL
+{
+    class Contexto : DbContext
+    {
+        public DbSet<Libros> libros { set; get; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
