8847473a7532f00c88bdc699c2c396eecc3da22b
DANNY BERNARD <DANNY.BERNARD@hotmail.com>
2018-09-13 04:09:25 +0200

    Empezamos

---
+++ RegistroDeBiblias/Entidades/Libros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDeBiblias.Entidades
+{
+    class Libros
+    {
+        [Key]
+        public int Id { set; get; } 
+        public string Nombre { set; get; }
+        public string Descripcion { set; get; }
+        public string Sigla { set; get; }
+        public string Tipo { set; get; }
+
+        public Libros()
+        {
+            Id = 0;
+            Nombre = string.Empty;
+            Descripcion = string.Empty;
+            Sigla = string.Empty;
+            Tipo = string.Empty;
+        }
+    }
+}
